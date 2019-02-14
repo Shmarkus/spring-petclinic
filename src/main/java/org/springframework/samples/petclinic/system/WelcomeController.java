c192fbda74e510249452fac89640a5f5dcd5b222
Markus Karileet <markus@helmes.com>
2019-02-14 15:02:19 +0200

    create placeholder

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -4,11 +4,20 @@ package org.springframework.samples.petclinic.system;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
 @Controller
 class WelcomeController {
 
     @GetMapping("/")
-    public String welcome() {
+    public String welcome(Map<String, Object> model) {
+        String temp = "9";
+        String min = "8";
+        String max = "7";
+
+        model.put("temp", temp);
+        model.put("min", min);
+        model.put("max", max);
         return "welcome";
     }
 }
